9f34b3cf9724b8ea0d3edfcaf3a8d3e50fd066f7
Guillaume_J <118889388+UltraFatPanda@users.noreply.github.com>
2024-06-26 22:33:22 +0200

    update

---
+++ 5- Cours React/TP3 Crypto watch/Ma version/crypto-watch/src/components/HeaderInfos.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const HeaderInfos = () => {
+  const [headerData, setHeaderData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.coingecko.com/api/v3/global")
+      .then((res) => setHeaderData(res.data.data));
+  }, []);
+
+  return (
+    <div className="header-container">
+      <ul className="title">
+        <li>
+          <h1>
+            <img src="./assets/logo.png" alt="" /> Watch Tower
+          </h1>
+        </li>
+        <li>
+          Crypto-monnaies :{" "}
+          {headerData.active_cryptocurrencies &&
+            headerData.active_cryptocurrencies.toLocaleString()}
+          {/* permet de dire attends que headerdata active truc existe pour que tu l'affiche ? */}
+        </li>
+        <li>Marchés : {headerData.markets && headerData.markets}</li>
+      </ul>
+      <ul className="infos-mkt">
+        <li className="global-mkt">Global Market Cap :</li>
+      </ul>
+    </div>
+  );
+};
+
+export default HeaderInfos;
