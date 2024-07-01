5fb7bd11dbe80be3b262af22f2e4e8e34857de36
Guillaume_J <118889388+UltraFatPanda@users.noreply.github.com>
2024-07-01 16:03:07 +0200

    update

--- 6- Backend JS/Full Stack OpenClassRoom/Readme.txt
+++ 6- Backend JS/Full Stack OpenClassRoom/Readme.txt
@@ -2,11 +2,16 @@ Le front est installé avec  npm install  , et lancé avec  npm run start
     Les projets Node sont initialisés avec la commande  npm init 
 
 
+
 Créez un fichier server.js à l'intérieur de votre dossier backend . Il contiendra votre premier serveur Node.
     Un serveur Node basique est démarré avec la méthode  createServer  du package  http  .
 
+
+
 npm install -g nodemon
-    nodemon server
+
+    nodemon server +++
+
         Nodemon est un package qui mettra à jour votre serveur démarré à chaque changement de fichier, vous facilitant le développement Node.
 
 
@@ -27,3 +32,36 @@ La méthode app.use() vous permet d'attribuer un middleware à une route spécif
     Le CORS définit comment les serveurs et les navigateurs interagissent, en spécifiant quelles ressources peuvent être demandées de manière légitime – par défaut, les requêtes AJAX sont interdites.
 
     Pour permettre des requêtes cross-origin (et empêcher des erreurs CORS), des headers spécifiques de contrôle d'accès doivent être précisés pour tous vos objets de réponse.
+
+
+npm install mongoose
+
+    Pour des applications qui ont besoin d'évoluer rapidement, les bases de données NoSQL comme MongoDB sont souvent un bon choix de technologie.
+
+    MongoDB Atlas permet d'héberger gratuitement une base de données MongoDB.
+
+    Le package Mongoose facilite les interactions entre votre application Express et votre base de données MongoDB.
+
+    const mongoose = require('mongoose');
+    mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+
+La méthode  Schema  de Mongoose vous permet de créer un schéma de données pour votre base de données MongoDB.
+
+    La méthode  model  transforme ce modèle en un modèle utilisable.
+
+
+Les méthodes de votre modèle Thing permettent d'interagir avec la base de données :
+
+    save()  – enregistre un Thing ;
+
+    find()  – retourne tous les Things ;
+
+    findOne()  – retourne un seul Thing basé sur la fonction de comparaison qu'on lui passe (souvent pour récupérer un Thing par son identifiant unique).
+
+    La méthode  app.get()  permet de réagir uniquement aux requêtes de type GET.
--- 6- Backend JS/Full Stack OpenClassRoom/backend/app.js
+++ 6- Backend JS/Full Stack OpenClassRoom/backend/app.js
@@ -1,6 +1,31 @@
-// const express = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
 
-// const app = express();
+const app = express();
+const Thing = require("./models/thing");
+
+mongoose
+  .connect(
+    "mongodb+srv://UltraFatPanda:UltraFatPanda0545699631@cluster0.p5gblyx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 // app.use((req, res, next) => {
 //   console.log("Requête reçue !");
@@ -21,7 +46,7 @@
 //   console.log("Réponse envoyée avec succès !");
 // });
 
-// module.exports = app;
+module.exports = app;
 // Cette application Express contient quatre éléments de middleware :
 
 // le premier enregistre « Requête reçue ! » dans la console et passe l'exécution ;
@@ -31,27 +56,42 @@
 // le troisième envoie la réponse JSON et passe l'exécution ;
 
 // le dernier élément de middleware enregistre « Réponse envoyée avec succès ! » dans la console.
-
-app.use("/api/stuff", (req, res, next) => {
-  const stuff = [
-    {
-      _id: "oeihfzeoi",
-      title: "Mon premier objet",
-      description: "Les infos de mon premier objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 4900,
-      userId: "qsomihvqios",
-    },
-    {
-      _id: "oeihfzeomoihi",
-      title: "Mon deuxième objet",
-      description: "Les infos de mon deuxième objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 2900,
-      userId: "qsomihvqios",
-    },
-  ];
-  res.status(200).json(stuff);
+app.post("/api/stuff", (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body,
+  });
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
 });
+
+// Ce modèle comporte une méthode save() qui enregistre simplement votre Thing dans la base de données.
+// La méthode save() renvoie une Promise. Ainsi, dans notre bloc then() , nous renverrons une réponse de réussite avec un code 201 de réussite. Dans notre bloc catch() , nous renverrons une réponse avec l'erreur générée par Mongoose ainsi qu'un code d'erreur 400
+
+app.get("/api/stuff/:id", (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }));
+});
+
+// nous utilisons la méthode get() pour répondre uniquement aux demandes GET à cet endpoint ;
+
+// nous utilisons deux-points : en face du segment dynamique de la route pour la rendre accessible en tant que paramètre ;
+
+// nous utilisons ensuite la méthode findOne() dans notre modèle Thing pour trouver le Thing unique ayant le même _id que le paramètre de la requête ;
+
+// ce Thing est ensuite retourné dans une Promise et envoyé au front-end ;
+
+// si aucun Thing n'est trouvé ou si une erreur se produit, nous envoyons une erreur 404 au front-end, avec l'erreur générée.
+
+app.get("/api/stuff", (req, res, next) => {
+  Thing.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// Dans l'exemple ci-dessus, nous utilisons la méthode find() dans notre modèle Mongoose afin de renvoyer un tableau contenant tous les Things dans notre base de données. À présent, si vous ajoutez un Thing , il doit s'afficher immédiatement sur votre page d'articles en vente.
+
+// En revanche, si vous cliquez sur l'un des Things , l'affichage d'un seul élément ne fonctionne pas. En effet, il tente d'effectuer un appel GET différent pour trouver un Thing individuel. Implémentons cette route maintenant.
